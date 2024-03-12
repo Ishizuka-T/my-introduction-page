7933c276bf1f15b75d4c4b3cbff96caa077e7710
Ishizuka-T <t_ishizuka@satt.jp>
2024-03-12 10:09:59 +0900

    最初のコミット

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>ishizukaのページ</h1>
+        <p>
+          おはようございます。
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
